c675f4e80f0df1bba632dcdffe701134c28bb276
lojb <balazsloj02@gmail.com>
2022-12-05 15:23:18 +0100

    allergens list added, get request for allergens working

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 const app = express();
+
+const allergensList = path.join(`${__dirname}/allergens.json`);
 
 const port = 9001;
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
+
+app.get("/api/pizza", (req, res) => {
+
+})
+
+app.get("/api/allergens", async (req, res) => {
+    const list = await fs.readFile(allergensList)
+
+    res.send(JSON.parse(list))
+})
+
+app.get("/pizza/list", (req, res) => {
+
+})
 
 
 
