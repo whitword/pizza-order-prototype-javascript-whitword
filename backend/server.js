3475c79761ab739c94b5904b5304455f7d62fc18
whitword <gyorgyi.lajterne@gmail.com>
2022-12-07 11:28:01 +0100

    Order handling

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,14 @@ const app = express();
 const port = 9001;
 
 app.use(express.static('frontend'));    //static files
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
 
 app.get("/api/pizza", (req, res) => {
-    fs.readFile(path.join(`${__dirname}/pizza.json`), (err, data) =>{
+    fs.readFile(path.join(`${__dirname}/pizza.json`), (err, data) => {
         if (err) {
             console.log(err);
             res.status(500).send("Error")
@@ -24,7 +25,7 @@ app.get("/api/pizza", (req, res) => {
 })
 
 app.get("/api/order", (req, res) => {
-    fs.readFile(path.join(`${__dirname}/order.json`), (err, data) =>{
+    fs.readFile(path.join(`${__dirname}/order.json`), (err, data) => {
         if (err) {
             console.log(err);
             res.status(500).send("Error")
@@ -36,7 +37,7 @@ app.get("/api/order", (req, res) => {
 })
 
 app.get("/api/allergens", (req, res) => {
-    fs.readFile(path.join(`${__dirname}/allergens.json`), (err, data) =>{
+    fs.readFile(path.join(`${__dirname}/allergens.json`), (err, data) => {
         if (err) {
             console.log(err);
             res.status(500).send("Error")
@@ -51,10 +52,31 @@ app.get("/pizza/list", (req, res) => {
     res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
 
-
-
-
-
-
+app.post("/api/order", (req, res) => {
+    if (!req.body) { res.status(400).send("Error"); }
+    else {
+        fs.readFile(path.join(`${__dirname}/order.json`), (err, orderData) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send("Error")
+            } else {
+                console.log("hello" +orderData)
+                let content = req.body;
+                // content = content.trim();
+                orderData = JSON.parse(orderData);
+                orderData.orders.push(content);
+                fs.writeFile(`${__dirname}/order.json`, JSON.stringify(orderData, null, 2), err => {
+                    if (err) {
+                        console.error(err);
+                    }
+                });
+                res.send("DONE");
+            }
+        })
+    }
+})
 
 app.listen(port, () => console.log(`http://127.0.0.1:${port}`));
+
+
+
--- frontend/script.js
+++ frontend/script.js
@@ -1,40 +1,143 @@
 const rootElement = document.createElement("div");
 rootElement.setAttribute("id", "root");
 document.body.appendChild(rootElement);
+const siteTitle = document.createElement('h1');
+siteTitle.innerText = "Welcome to Nonna Pizza";
+document.body.insertAdjacentElement("afterbegin", siteTitle);
+
+const buttonComponent = (id, text) => `<button id="${id}">${text}</button>`;
+const createLink = (url, text) => `<a class="btnLikeLink" href="${url}">${text}</a>`;
+const inputComponent = (id, plch) => `<input id="${id}" placeholder = "${plch}"></input>`;
+
+function loadSite() {
+  rootElement.insertAdjacentHTML("afterbegin", createLink("http://127.0.0.1:9001/pizza/list", "Our Pizzas"));
+}
 
 if (document.URL == 'http://127.0.0.1:9001/') {
-    const buttonComponent = (id, text) => `<button id="${id}">${text}</button>`;
-    const createLink = (url, text) => `<a class="btnLikeLink" href="${url}">${text}</a>`;
-
-    function loadSite() {
-        rootElement.insertAdjacentHTML("afterbegin", createLink("http://127.0.0.1:9001/pizza/list", "Our Pizzas"));
-    }
-    loadSite();
+  loadSite();
 }
 
 if (document.URL == 'http://127.0.0.1:9001/pizza/list') {
-    console.log('pizza list');
+  console.log('pizza list');
 
-    fetch('/../api/pizza').then((response) => {
-        response.json().then((data) => {
-            data.map(pizzaID => {
-                console.log(pizzaID);
+  fetch('/../api/pizza').then((response) => {
+    response.json().then((data) => {
+      data.map(pizzaID => {
+        console.log(pizzaID);
 
-                const pizzaDiv = document.createElement("div")
-                rootElement.appendChild(pizzaDiv)
+        const pizzaDiv = document.createElement("div")
+        rootElement.appendChild(pizzaDiv)
 
-                pizzaDiv.setAttribute("id", `${pizzaID.name}`)
+        pizzaDiv.setAttribute("id", `${pizzaID.name}`)
 
-                for (const [key, value] of Object.entries(pizzaID)) {
-                    console.log(value);
-                    const div = document.createElement("div")
-                    pizzaDiv.appendChild(div)
+        for (const [key, value] of Object.entries(pizzaID)) {
+          console.log(value);
+          const div = document.createElement("div")
+          pizzaDiv.appendChild(div)
 
-                    div.setAttribute("id", `${key}`)
+          div.setAttribute("id", `${key}`)
 
-                    div.innerHTML = `${key}: ${value}`
-                }
-            })
+          div.innerHTML = `${key}: ${value}`
+        }
+
+        pizzaDiv.insertAdjacentHTML("afterbegin", buttonComponent(`orderBtn${pizzaID.id}`, "Add to cart"));
+        document.getElementById(`orderBtn${pizzaID.id}`).addEventListener('click', function () {
+          document.getElementById('orderForm').style.display = "block"
+        })
+        pizzaDiv.insertAdjacentHTML("afterbegin", inputComponent(`${pizzaID.id}`, "Order amount"));
+        document.getElementById(`${pizzaID.id}`).addEventListener('input', function (e) {
+          const pizzaToOrder = transferObj.pizzas.find(i => i.id == e.target.id)
+          if (pizzaToOrder) {
+            pizzaToOrder.amount = +e.target.value
+          } else {
+            transferObj.pizzas.push({ id: +e.target.id, amount: e.target.value })
+          }
         });
+        console.log(transferObj);
+      })
     });
+  })
+    .catch(err => console.log(err));
 }
+
+
+let orderSchema = {
+  id: 0,
+  pizzas: [
+    {
+      id: 0,
+      amount: 0
+    }
+  ],
+  date: {
+    year: 0000,
+    month: 0,
+    day: 0,
+    time: "00:00"
+  },
+  customer: {
+    name: "",
+    email: "",
+    address: {
+      city: "",
+      street: ""
+    }
+  }
+}
+
+let transferObj = JSON.parse(JSON.stringify(orderSchema));
+
+const formComponent = () => `
+	<form id="orderForm" style="display:none">     
+    <h2>Your personal data</h2>
+		<input type=text id="nameInput" placeholder="Your name"></input>
+		<input type=text id="email" placeholder="your email"></input>
+        <h2>Your Address</h2>
+		<input type=text id="city" placeholder="City"></input>
+		<input type=text id="street" placeholder="Street"></input>
+    <button id="submit">Submit</button>
+	</form>
+`;
+rootElement.insertAdjacentHTML("afterbegin", formComponent());
+document.getElementById('nameInput').addEventListener('input', (e) => transferObj.customer.name = e.target.value)
+document.getElementById('email').addEventListener('input', (e) => transferObj.customer.email = e.target.value)
+document.getElementById('city').addEventListener('input', (e) => transferObj.customer.address.city = e.target.value)
+document.getElementById('street').addEventListener('input', (e) => transferObj.customer.address.street = e.target.value)
+document.getElementById('submit').addEventListener('click', function (event) {
+  event.preventDefault();
+  orderGet();
+});
+
+function orderGet() {
+  fetch('/api/order')
+    .then((response) => response.json())
+    .then(jsonData => {
+      data = jsonData;
+      transferObj.id = data.length + 1;
+      let utc = new Date().toJSON()
+      transferObj.date.year = utc.slice(0, 4);
+      transferObj.date.month = utc.slice(5, 7);
+      transferObj.date.day = utc.slice(8, 10);
+      transferObj.date.time = utc.slice(11, 17);
+      console.log(transferObj);
+      console.log(JSON.stringify(transferObj));
+    })
+    .then(x => orderPost())
+}
+function orderPost() {
+  fetch('/api/order', {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json'
+    },
+    body:
+      JSON.stringify(transferObj)
+  })
+    // .then((response) => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => console.log(err));
+}
+
