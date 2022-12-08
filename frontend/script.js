5fe8e8bdde863a3bd17f50b4a342b216bb82fe65
lojb <balazsloj02@gmail.com>
2022-12-08 13:15:42 +0100

    site working

--- frontend/script.js
+++ frontend/script.js
@@ -12,8 +12,8 @@ const inputComponent = (id, plch) => `<input id="${id}" placeholder = "${plch}">
 
 function loadHeader() {
   header.insertAdjacentHTML("beforeend", createLink("http://127.0.0.1:9001/pizza/list", "Our Pizzas \n"));
-  header.insertAdjacentHTML("beforeend", createLink("http://127.0.0.1:9001/pizza/allergens", "Allergens \n"));
   header.insertAdjacentHTML("beforeend", createLink("http://127.0.0.1:9001/", "NONNA PIZZA \n"));
+  header.insertAdjacentHTML("beforeend", createLink("http://127.0.0.1:9001/pizza/allergens", "Allergens \n"));
   //header.insertAdjacentHTML("beforeend", createLink("http://127.0.0.1:9001/order", "Your Cart \n"));
 }
 
@@ -69,6 +69,8 @@ const formComponent = () => `
           <h2>Your Address</h2>
       <input type=text id="city" placeholder="City"></input>
       <input type=text id="street" placeholder="Street"></input>
+      <h2>Your order</h2>
+      <div id="yourOrder"></div>
       <button id="submit">Submit</button>
     </form>
   `;
@@ -82,56 +84,25 @@ document.getElementById('submit').addEventListener('click', function (event) {
   orderGet();
 });
 
-/*if (document.URL == 'http://127.0.0.1:9001/order') {
-  loadHeader();
-  loadOrderPage()
-  console.log(transferObj);
-}*/
-
-function loadOrderPage() {
-  const formComponent = () =>
-    `	<form id="orderForm" style="display:block">     
-    <h2>Your personal data</h2>
-		<input type=text id="nameInput" placeholder="Your name"></input>
-		<input type=text id="email" placeholder="your email"></input>
-        <h2>Your Address</h2>
-		<input type=text id="city" placeholder="City"></input>
-		<input type=text id="street" placeholder="Street"></input>
-    <button id="submit">Submit</button>
-	</form>`;
-
-  rootElement.insertAdjacentHTML("afterbegin", formComponent());
-
-  document.getElementById('nameInput').addEventListener('input', (e) => transferObj.customer.name = e.target.value)
-  document.getElementById('email').addEventListener('input', (e) => transferObj.customer.email = e.target.value)
-  document.getElementById('city').addEventListener('input', (e) => transferObj.customer.address.city = e.target.value)
-  document.getElementById('street').addEventListener('input', (e) => transferObj.customer.address.street = e.target.value)
-  document.getElementById('submit').addEventListener('click', function (event) {
-    event.preventDefault();
-    orderGet();
-  });
-
-}
-function loadAllergens(){
+function loadAllergens() {
   fetch('/../api/allergens')
-  .then((response) => {
-    response.json()
-    .then((data) => {
-      console.log(data);
-      data.allergens.map(i => {
-        const allergen = document.createElement("div")
-        rootElement.appendChild(allergen)
-        allergen.setAttribute("id", "allergenContainer")
+    .then((response) => {
+      response.json()
+        .then((data) => {
+          data.allergens.map(i => {
+            const allergen = document.createElement("div")
+            rootElement.appendChild(allergen)
+            allergen.setAttribute("id", "allergenContainer")
 
-        for (const [key, value] of Object.entries(i)) {
-            const div = document.createElement("div")
-            div.setAttribute("id", "allergens")
-            div.innerHTML = `${key}: ${value}`
-            allergen.appendChild(div)
-          }
-      })
-})
-})
+            for (const [key, value] of Object.entries(i)) {
+              const div = document.createElement("div")
+              div.setAttribute("id", "allergens")
+              div.innerHTML = `${key}: ${value}`
+              allergen.appendChild(div)
+            }
+          })
+        })
+    })
 };
 
 function loadPizzaList() {
@@ -169,66 +140,94 @@ function loadPizzaList() {
           if (e.target.value > 0) {
             document.getElementById(`orderBtn${pizzaID.id}`).disabled = false;
           }
-          /*if (e.target.value > 0) {
-            document.getElementById(`orderBtn${pizzaID.id}`).disabled = false;
-            if (pizzaToOrder) {
-              pizzaToOrder.amount = +e.target.value
-            } else {
-              transferObj.pizzas.push({ id: +e.target.id, amount: e.target.value })
-              console.log(transferObj);
-            }
-          } else if (e.target.value == 0 && pizzaToOrder) {
-            let pizzaToDelete = transferObj.pizzas.findIndex(i => i.id == e.target.id)
-            transferObj.pizzas.splice(pizzaToDelete, 1);
-          }
-          if (transferObj.pizzas.length < 1) {
-            document.getElementById('orderForm').style.display = "none";
-            document.getElementById(`orderBtn${pizzaID.id}`).disabled = true;*/
+          
         });
 
-      document.getElementById(`orderBtn${pizzaID.id}`).addEventListener('click', function () {
-        document.getElementById('orderForm').style.display = "block"
+        document.getElementById(`orderBtn${pizzaID.id}`).addEventListener('click', function () {
+          document.getElementById('orderForm').style.display = "block"
 
-        const inputField = document.getElementById(`${pizzaID.id}`)
+          const inputField = document.getElementById(`${pizzaID.id}`)
 
-        const pizzaToOrder = transferObj.pizzas.find(i => i.id == pizzaID.id)
+          const pizzaToOrder = transferObj.pizzas.find(i => i.id == pizzaID.id)
 
-        if (pizzaToOrder) {
-          pizzaToOrder.amount = +inputField.value
-        } else {
-          transferObj.pizzas.push({ id: +pizzaID.id, amount: inputField.value })
-          console.log(transferObj);
-        }
-        if (inputField.value == 0 && pizzaToOrder) {
-          let pizzaToDelete = transferObj.pizzas.findIndex(i => i.id == inputField.id)
-          transferObj.pizzas.splice(pizzaToDelete, 1);
-        }
-        if (transferObj.pizzas.length < 1) {
-          document.getElementById('orderForm').style.display = "none";
-          document.getElementById(`orderBtn${pizzaID.id}`).disabled = true;
-        }
+          const yourOrder = document.getElementById("yourOrder");
 
-        /*const pizzaToOrder = transferObj.pizzas.find(i => i.id == e.target.id)
-        if (e.target.value > 0) {
-        document.getElementById(`orderBtn${pizzaID.id}`).disabled = false;
-          if (pizzaToOrder) {
-            pizzaToOrder.amount = +e.target.value
+          if (!pizzaToOrder) {
+            const p = document.createElement("p")
+            p.setAttribute("id", `orderOf${pizzaID.id}`)
+            yourOrder.appendChild(p)
+            const yourPizza = document.getElementById(`orderOf${pizzaID.id}`)
+            transferObj.pizzas.push({ id: +pizzaID.id, amount: inputField.value })
+
+            yourPizza.innerText += `${pizzaID.name}: ${inputField.value}`
+            addButtons()
           } else {
-            transferObj.pizzas.push({ id: +e.target.id, amount: e.target.value })
-            console.log(transferObj);
+            const yourPizza = document.getElementById(`orderOf${pizzaID.id}`)
+
+            pizzaToOrder.amount = +inputField.value
+
+            yourPizza.innerText = `${pizzaID.name}: ${inputField.value}`
+            addButtons()
           }
-        } else if (e.target.value == 0 && pizzaToOrder) {
-          let pizzaToDelete = transferObj.pizzas.findIndex(i => i.id == e.target.id)
-          transferObj.pizzas.splice(pizzaToDelete, 1);
-        }
-        if (transferObj.pizzas.length < 1) {
-          document.getElementById('orderForm').style.display = "none";
-          document.getElementById(`orderBtn${pizzaID.id}`).disabled = true;
-        }*/
+
+          const yourPizza = document.getElementById(`orderOf${pizzaID.id}`)
+
+          checkIfDelete()
+          function checkIfDelete() {
+            const pizzaToOrder = transferObj.pizzas.find(i => i.id == pizzaID.id)
+            
+            if ((inputField.value == 0 || pizzaToOrder.amount == 0) && pizzaToOrder) {
+              let pizzaToDelete = transferObj.pizzas.findIndex(i => i.id == inputField.id)
+              transferObj.pizzas.splice(pizzaToDelete, 1);
+
+              yourOrder.removeChild(yourPizza)
+            }
+
+            if (transferObj.pizzas.length < 1) {
+              document.getElementById('orderForm').style.display = "none";
+              document.getElementById(`orderBtn${pizzaID.id}`).disabled = true;
+            }
+          }
+
+          function addButtons() {
+            const yourPizza = document.getElementById(`orderOf${pizzaID.id}`)
+            const minusButton = document.createElement("button")
+            const pizzaToOrder = transferObj.pizzas.find(i => i.id == pizzaID.id)
+
+            minusButton.setAttribute("id", `minus${pizzaID.id}`)
+            minusButton.innerText = '-'
+            yourPizza.appendChild(minusButton)
+
+            const plusButton = document.createElement("button")
+            plusButton.setAttribute("id", `plus${pizzaID.id}`)
+            plusButton.innerText = '+'
+            yourPizza.appendChild(plusButton)
+
+            minusButton.addEventListener("click", function (event) {
+              event.preventDefault();
+
+              pizzaToOrder.amount--
+              yourPizza.innerText = `${pizzaID.name}: ${pizzaToOrder.amount}`
+              inputField.value = pizzaToOrder.amount
+              addButtons()
+              checkIfDelete()
+            })
+  
+            plusButton.addEventListener("click", function (event) {
+              event.preventDefault();
+  
+              pizzaToOrder.amount++
+              yourPizza.innerText = `${pizzaID.name}: ${pizzaToOrder.amount}`
+              inputField.value = pizzaToOrder.amount
+              addButtons()
+              checkIfDelete()
+            })
+          }
+
+        })
       })
     })
   })
-})
 }
 
 function orderGet() {
@@ -241,7 +240,7 @@ function orderGet() {
       transferObj.date.year = utc.slice(0, 4);
       transferObj.date.month = utc.slice(5, 7);
       transferObj.date.day = utc.slice(8, 10);
-      transferObj.date.time = utc.slice(11, 17);
+      transferObj.date.time = utc.slice(11, 16);
     })
     .then(x => orderPost())
 }
@@ -255,9 +254,8 @@ function orderPost() {
     body:
       JSON.stringify(transferObj)
   })
-    // .then((response) => response.json())
     .then(data => {
-      console.log(data);
+      //console.log(data);
     })
     .catch(err => console.log(err));
 }
